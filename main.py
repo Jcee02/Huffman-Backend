9b02bd4e7f2d94ebb82368adf7d049d554208c13
Juan Carlos Torres Gutiérrez <118766109+Jcee02@users.noreply.github.com>
2024-04-30 19:57:58 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,69 +2,144 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
-from collections import Counter
 import os
-
-#Ventana
+import src.huffman as hf
+import pickle
+# Ventana
 window = tk.Tk()
+
+
+ABS_PATH = os.getcwd()
+
+i_f = "input.txt"
+o_f = "output.bin"
+
 
 def ventana_bienvenida():
     for widget in window.winfo_children():
-        widget.destroy() #Esta funcion borra el contenido del widget
+        widget.destroy()  # Esta funcion borra el contenido del widget
     ventana()
-    ttk.Label(window, text="Bienvenido a la Actividad 07 - AGC").place(x=525, y=50) #Etiqueta pricipal mensaje
-    ttk.Label(window, text="Selecciona Un Archivo").place(x=557, y=100) #mensaje de seleccionar archivo
+    ttk.Label(window, text="Bienvenido a la Actividad 07 - AGC").place(x=525, y=50)  # Etiqueta pricipal mensaje
+    ttk.Label(window, text="Selecciona Un Archivo").place(x=557, y=100)  # mensaje de seleccionar archivo
 
 
-def Abrir_Archivo(): #Función Seleccionar archivo
-    filepath = askopenfilename(initialdir="/", title="Seleccion de Archivo", filetypes=(("Text files", "*.txt"), ("Todos los Archivos", "*.*"))) # Abre el explorador de archivos, filtrando solo archivos txt   
-    return filepath #Retorna el archivo seleccionado
+def Abrir_Archivo():  # Función Seleccionar archivo
+    filepath = askopenfilename(initialdir="/", title="Seleccion de Archivo", filetypes=(("Text files", "*.txt"), ("Todos los Archivos", "*.*")))  # Abre el explorador de archivos, filtrando solo archivos txt   
+    return filepath  # Retorna el archivo seleccionado
 
 
-def ventana_archivo_abierto():
+def Abrir_BIN():
+    filepath = askopenfilename(initialdir="/", title="Seleccion de Archivo", filetypes=(("Binary files", "*.bin"), ("Todos los Archivos", ".")))
+    return filepath
+
+
+def huff_txt():
     for widget in window.winfo_children():
-        widget.destroy() #Esta funcion borra el contenido del widget
+        widget.destroy()
     ventana()
-    filepath = Abrir_Archivo() #Llama a la función Abrir_Archivo
-    filename = os.path.basename(filepath) #Obtiene el nombre del archivo seleccionado
-    archivos = ttk.Label(window, text="Seleccione Archivo:") #Etiqueta de seleccionar archivo
-    archivos.place(x=555, y=50) #posicion de la etiqueta archivo
-    archivos.configure(text="Archivo Abierto: \n\n"+     filename)#realiza la apertura del explorador de archivos y lo limita a los tipos .xlsx y csv
+    filepath = Abrir_Archivo()
+    filename = os.path.basename(filepath)
+    archivos = ttk.Label(window, text="Seleccione Archivo:")
 
-    with open(filepath, 'r') as file: #Abre el archivo seleccionado en modo lectura
-        texto = file.read() #Lee el contenido del archivo
-    lista = Counter(texto).most_common() #Cuenta la cantidad de caracteres en el archivo y el numero de veces que se repiten
-    diccionario = dict(lista) #Convierte la lista en un diccionario
+    archivos.place(x=555, y=50)
+    archivos.configure(text="Archivo abierto: \n\n"+filename)
+    
+    freq_list = hf.generate_dict(filename)
+    freq_list = sorted(freq_list.items(), key=lambda x: x[1], reverse=True)
+
+    nodes = freq_list
+
+    while len(nodes) > 1:
+        (key1, c1) = nodes[-1]
+        (key2, c2) = nodes[-2]
+        nodes = nodes[:-2]
+        
+        node = hf.NodeTree(key1, key2)
+        nodes.append((node, c1 + c2))
+
+        nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+
+    huffman_code = hf.huffman_code_tree(nodes[0][0])
+
+    output = tk.Text(window)
+    output.pack(expand=True, fill="x")
+    
+    output.insert(tk.END, ' Char | Huffman Code')
+    output.insert(tk.END, '--------------------')
+    for (char, frequency) in freq_list:
+        output.insert(tk.END, ' %-4r |%12s' % (char, huffman_code[char]))
+    output.see()
+
+def compress_txt():
+    for widget in window.winfo_children():
+        widget.destroy()  # Esta funcion borra el contenido del widget
+    ventana()
+    filepath = Abrir_Archivo()  # Llama a la función Abrir_Archivo
+    filename = os.path.basename(filepath)  # Obtiene el nombre del archivo seleccionado
+    archivos = ttk.Label(window, text="Seleccione Archivo:")  # Etiqueta de seleccionar archivo
+    archivos.place(x=555, y=50)  # posicion de la etiqueta archivo
+    archivos.configure(text="Archivo comprimido: \n\n"+filename)  # realiza la apertura del explorador de archivos y lo limita a los tipos .xlsx y csv
+
+    input_f = open(ABS_PATH+"//"+filename, "rb").read()
+    output_f = open(ABS_PATH+"//"+o_f, "wb")
+    
+    compressed_file = hf.compress(input_f)
+    pickle.dump(compressed_file, output_f)
 
     # Crea un widget de texto
-    output = tk.Text(window) 
+    output = tk.Text(window)
     output.pack(expand=True, fill="x")
-    # Se inserta el texto en el widget de texto 
-    for key, value in diccionario.items(): #Recorre el diccionario y lo imprime en el widget de texto en formato de lista
-        output.insert(tk.END, f'\n{key}: {value}\n') #Imprime el resultado en el widget de texto
-    output.see() # Muestra el resultado en el widget de texto
+    # Se inserta el texto en el widget de texto
+    output.insert(tk.END, f'Compressed file in {o_f}')  # Imprime el resultado en el widget de texto
+    output.see()  # Muestra el resultado en el widget de texto
+
+
+def decompress_bin():
+    for widget in window.winfo_children():
+        widget.destroy()  # Esta funcion borra el contenido del widget
+    ventana()
+    filepath = Abrir_BIN()  # Llama a la función Abrir_Archivo
+    filename = os.path.basename(filepath)  # Obtiene el nombre del archivo seleccionado
+    archivos = ttk.Label(window, text="Seleccione Archivo:")  # Etiqueta de seleccionar archivo
+    archivos.place(x=555, y=50)  # posicion de la etiqueta archivo
+    archivos.configure(text="Archivo descomprimido: \n\n"+filename)  # realiza la apertura del explorador de archivos y lo limita a los tipos .xlsx y csv
+
+    input_f = pickle.load(open(ABS_PATH+"//"+filename, "rb"))
+    output_f = open(ABS_PATH+"//"+i_f, "w")
+
+    uncompressed_file = hf.decompress(input_f)
+
+    for l in uncompressed_file:
+        output_f.write()
+    output_f.close()
     
+        # Crea un widget de texto
+    output = tk.Text(window)
+    output.pack(expand=True, fill="x")
+    # Se inserta el texto en el widget de texto
+    output.insert(tk.END, f'Decompressed file in {i_f}')  # Imprime el resultado en el widget de texto
+    output.see()  # Muestra el resultado en el widget de texto
+
+
+
 
 def ventana():
     for widget in window.winfo_children():
-        widget.destroy() #Esta funcion borra el contenido del widget
+        widget.destroy()  # Esta funcion borra el contenido del widget
 
     button_frame = tk.Frame(window)
     button_frame.pack(side=tk.BOTTOM, padx=10, pady=10)
-    # Boton Examinar 
-    examinar_button = tk.Button(button_frame, text="Examinar", command=ventana_archivo_abierto)
+    # Boton Examinar
+    examinar_button = tk.Button(button_frame, text="Examinar", command=huff_txt)
     examinar_button.pack(side=tk.RIGHT, padx=5)
-    #Boton Comprimir
-    comprimir_button = tk.Button(button_frame, text="Comprimir")
+    # Boton Comprimir
+    comprimir_button = tk.Button(button_frame, text="Comprimir", command=compress_txt)
     comprimir_button.pack(side=tk.RIGHT, padx=5)
     # Boton Descomprimir
-    descomprimir_button = tk.Button(button_frame, text="Descomprimir")
+    descomprimir_button = tk.Button(button_frame, text="Descomprimir", command=decompress_bin)
     descomprimir_button.pack(side=tk.RIGHT, padx=5)
 
-window.title("Actividad 07 - AGC") # Titulo en la ventana 
-window.geometry("1200x700") # Tamaño de la ventana 
+window.title("Actividad 07 - AGC")  # Titulo en la ventana 
+window.geometry("1200x700")  # Tamaño de la ventana
 ventana_bienvenida()
-window.mainloop() # Mainloop para correr ventana
-
-
-
+window.mainloop()  # Mainloop para correr ventana
--- src/huffman.py
+++ src/huffman.py
@@ -1,11 +1,114 @@
 from collections import Counter
 
-#Subroutines for main.py
+DICTIONARY_SIZE = 256
+# Subroutines for main.py
+
+
+# Wrapper for generate_dict for Tree construction instances
+# Decorating generate_dict function extracts ordered frequency of given chars
+def freq_decorator(dict):
+    def wrapper():
+        dummy_dict = dict
+        freq = list(dummy_dict.values())
+        print("Extracting ordered frequency list from Huffman dictionary")
+        return freq
+    return wrapper
+
 
 def generate_dict(filepath) -> dict:
     with open(filepath, 'r') as file:
         text = file.read()
 
-    #Generate char occurrence list and cast it to dict
+    # Generate char occurrence list and cast it to dict
     enum_dict = dict(Counter(text).most_common())
-    
+    return enum_dict
+
+
+
+
+# Tree related:
+
+
+class NodeTree(object):
+
+    def __init__(self, right=None, left=None):
+        self.left = left
+        self.right = right
+
+    def children(self):
+        return (self.left, self.right)
+
+    def nodes(self):
+        return (self.left, self.right)
+
+    def __str__(self):
+        return '%s_%s' % (self.left, self.right)
+
+
+def huffman_code_tree(node, left=True, binString=''):
+    # size of binString should be more than 1 for it to be str
+    if type(node) is str:
+        return {node: binString}
+
+    (l, r) = node.children()
+    d = dict()
+
+    d.update(huffman_code_tree(l, True, binString + '0'))
+    d.update(huffman_code_tree(r, False, binString + '1'))
+    return d
+
+def compress(input):
+    global DICTIONARY_SIZE
+    dictionary = {}
+    result = []
+    temp = ""
+
+    for i in range(0, DICTIONARY_SIZE):
+        dictionary[str(chr(i))] = i
+
+    for c in input:
+        temp2 = temp+str(chr(c))
+        if temp2 in dictionary.keys():
+            temp = temp2
+        else:
+            result.append(dictionary[temp])
+            dictionary[temp2] = DICTIONARY_SIZE
+            DICTIONARY_SIZE += 1
+            temp = ""+str(chr(c))
+
+
+    if temp != "":
+        result.append(dictionary[temp])  
+        
+    return result
+
+
+def decompress(input):
+    global DICTIONARY_SIZE
+    dictionary = {}
+    result = []
+
+    for i in range(0, DICTIONARY_SIZE):
+        dictionary[i] = str(chr(i))
+
+    previous = chr(input[0])
+    input = input[1:]
+    result.append(previous)
+
+    for bit in input:
+        aux = ""
+        if bit in dictionary.keys():
+            aux = dictionary[bit]
+        else:
+            aux = previous+previous[0] 
+            #Bit is not in the dictionary
+                 # Get the last character printed + the first position of the last character printed
+                 #because we must decode bits that are not present in the dictionary, so we have to guess what it represents, for example:
+                 #let's say bit 37768 is not in the dictionary, so we get the last character printed, for example it was 'uh'
+                 #and we take it 'uh' plus its first position 'u', resulting in 'uhu', which is the representation of bit 37768
+                 #the only case where this can happen is if the substring starts and ends with the same character ("uhuhu").
+        result.append(aux)
+        dictionary[DICTIONARY_SIZE] = previous + aux[0]
+        DICTIONARY_SIZE+= 1
+        previous = aux
+    return result
